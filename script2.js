fd5a0aa59838e73c30df30dd3e4ade0b1d2de58a
karan-suthar03 <116728069+karan-suthar03@users.noreply.github.com>
2023-03-14 16:32:48 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -21,11 +21,11 @@ fileInput.addEventListener('change', function(e) {
     var uploadTask = storageRef.child(newFileName).put(file);
     uploadTask.on('state_changed', function(snapshot) {
         var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //    console.log('Upload is ' + progress + '% done');
+    
     }, function(error) {
-    //    console.log(error);
+    
     }, function() {
-    //    console.log('Upload complete');
+    
     });
 });
 
